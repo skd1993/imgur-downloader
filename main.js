32e930875a9f2416eec0d5dee87ea2094a1ee09b
shobhit_kumar <shobhit.kr@outlook.com>
2017-04-04 13:07:21 +0530

    using imgur api now to fetch gallery images info

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const { app, BrowserWindow } = electron;
 
 var request = require('request');
 var fs = require('fs');
-var cheerio = require('cheerio');
 
 var mainWindow = null;
 var urls = [];
@@ -26,24 +25,62 @@ function search() {
     document.getElementById('inner').innerHTML = searchText;
     s = searchText;
     console.log(s);
-    request(s, function(err, resp, body) {
-        console.log('here');
-        if (!err && resp.statusCode == 200) {
-            var $ = cheerio.load(body);
-            $('.post-image-placeholder').each(function() {
-                console.log(this);
-                var url = this.attribs.src;
-                urls.push('http:\/\/' + url.substr(2));
-                // if (url.indexOf('i.imgur.com') != -1) {
-                //     urls.push(url);
-                // }
-            });
-            console.log(urls);
-            for (var i = 0; i < urls.length; i++) {
-                request(urls[i]).pipe(fs.createWriteStream('app/img/' + i + '.png'));
-            }
-        } else {
-            console.log(err);
+
+    is_gallery = (s.indexOf('gallery') < 0) ? false : true;
+
+    document.getElementById('inner').innerHTML = 'is this a gallery? ==> ' + is_gallery;
+
+    refine = s.substr(s.lastIndexOf('/') + 1);
+
+    console.log('the id of album: ' + refine);
+
+    request_url = 'http://api.imgur.com/3/album/' + refine + '/images';
+
+    console.log(request_url);
+
+    var options = {
+        url: s,
+        headers: {
+            'Authorization': 'Client-ID xxxxxxxxxxx'
         }
-    });
+    };
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body);
+            // console.log(response);
+            console.log(info.data);
+            console.log(info.data.images.length);
+            console.log(info.data.images);
+            for(var i=0; i<info.data.images.length; i++){
+                var html = '<li>' + info.data.images[i].link + '</li>';
+                document.getElementById('listofimages').innerHTML += html;
+            }
+        }
+        else {
+            cosole.log('some error occured');
+        }
+    }
+
+    request(options, callback);
+
+    // request(s, function(err, resp, body) {
+    //     console.log('here');
+    //     if (!err && resp.statusCode == 200) {
+    //         $('.post-image-placeholder').each(function() {
+    //             console.log(this);
+    //             var url = this.attribs.src;
+    //             urls.push('http:\/\/' + url.substr(2));
+    //             // if (url.indexOf('i.imgur.com') != -1) {
+    //             //     urls.push(url);
+    //             // }
+    //         });
+    //         console.log(urls);
+    //         for (var i = 0; i < urls.length; i++) {
+    //             request(urls[i]).pipe(fs.createWriteStream('app/img/' + i + '.png'));
+    //         }
+    //     } else {
+    //         console.log(err);
+    //     }
+    // });
 }
